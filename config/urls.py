b349d580945df35099945c1d5b5d613afe945edc
Jaeeon Lee <dannylee88@naver.com>
2023-09-27 14:13:13 +0900

    add index page

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
+
+# 미디어 파일 업로드 위해 추가
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", views.IndexView.as_view(), name="index"),
 ]
+
+app_name = "root"
+
+# 미디어 파일 업로드 위해 추가
+urlpatterns += static(
+    prefix = settings.MEDIA_URL,
+    document_root = settings.MEDIA_ROOT,
+)
