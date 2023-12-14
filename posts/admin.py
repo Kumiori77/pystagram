3d29035193f539d1e03da720c1a7b0a9c05c1dc1
Jaeeon Lee <dannylee88@naver.com>
2023-12-14 12:49:43 +0900

    add Post admin class

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from . import models
+
 
 # Register your models here.
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["id", "content"]
+
+@admin.register(models.PostImage)
+class PostImageAdmin(admin.ModelAdmin):
+    list_display = ["id", "post", "photo"]
+
+@admin.register(models.Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["id", "post", "content"]
+
